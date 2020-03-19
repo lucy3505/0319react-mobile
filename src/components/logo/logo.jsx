bc64826cd4d5ee4bbe0a85a8d6f03757d877893d
xuyanyu <13022112163@163.com>
2020-03-19 18:06:40 +0800

    init

---
+++ src/components/logo/logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './logo.less';
+
+export default function() {
+	return (
+		<div className="logo-container">
+			<img src={logo} alt="" className="logo-img" />
+		</div>
+	);
+}
