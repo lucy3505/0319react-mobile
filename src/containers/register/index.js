2697bd75222e1c52d42e0b2a35c836106cc50fdb
lucy3505 <415492767@qq.com>
2020-03-21 13:49:35 +0800

    'register'

--- src/containers/register/index.js
+++ src/containers/register/index.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react'
 import Logo from './../../components/logo/logo.jsx'
-import { List, InputItem, Radio, WingBlank } from 'antd-mobile';
+import { List, InputItem, Radio, WingBlank, WhiteSpace, Button } from 'antd-mobile';
+import styles from './index.less'
 const RadioItem = Radio.RadioItem;
+const ListItem = List.Item
 export default class index extends Component {
+  state = { radio: '' }
+
+  onChange = (radio) => {
+    this.setState({
+      radio
+    })
+  }
+
+
   render() {
+    const { radio } = this.state
+    const textTitle = "请选择职位"
     const data = [
-      { label: 'dashen' },
-      { label: 'laoban' },
+      { value: 'dashen', label: "boss" },
+      { value: 'laoban', label: "employee" },
     ];
     return (
       <div>
         <Logo />
+        <WhiteSpace />
         <WingBlank>
           <InputItem placeholder="请输入用户名">
             用户名：
@@ -21,11 +35,21 @@ export default class index extends Component {
           <InputItem>
             用户名：
           </InputItem>
-          {data.map(i => (
-            <Radio key={i.label} >
-              {i.label}
-            </Radio>
-          ))}
+          <ListItem >
+            <span>请选择职位：</span>
+
+            {data.map(i => (
+              <Radio className={styles.myRadio} key={i.value} checked={radio === i.value} onChange={() => this.onChange(i.value)} >
+                {i.label}
+              </Radio>
+            ))}
+
+          </ListItem>
+          <WhiteSpace />
+          <Button type="primary">注册</Button>
+          <WhiteSpace />
+          <Button>已有账户</Button>
+
         </WingBlank>
       </div>
     )
